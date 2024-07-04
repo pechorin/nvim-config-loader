1a3b88de13b6c70a0b193977fbcef3d7815560fa
andrey pechorin <pechorin.andrey@gmail.com>
2024-07-05 02:46:03 +0300

    updates

--- nvim-config-loader.lua
+++ nvim-config-loader.lua
@@ -1,28 +1,36 @@
--- TODO: lazy.nvim integration ?
+-- TODO: lazy.nvim/vim-pack integration ?
 -- TODO: add before|after_setup ?
--- TODO: add required plugins
---       * alpha nvim
---       * nvin-lint
+-- TODO:
 
 function _G.dump(...)
   local objects = vim.tbl_map(vim.inspect, {...})
   print(unpack(objects))
 end
 
-TinyNvimLoader = {
+local NvimConfigLoader = {
   profile_loads        = 0,
 
   colorscheme          = 'default',
   bg                   = 'light',
 
-  vim_plug_bundle_path = nil,
-  main_settings_files  = {},
+  -- Vim plug bundle path
+  vim_plug_bundle_path = '~/.vim/bundle',
 
-  use_rg               = false,
-  use_zsh              = false,
-  use_fzf              = false,
+  -- Additional .vim or .lua config sources from ~/.vim/ like:
+  --  ~/.vim/completion.lua
+  --  ~/.vim/lsp.lua
+  --
+  -- @example
+  --  additional_config_files = { 'lsp.lua' }
+  additional_config_files = {},
+
+  -- TODO: RENAME use_nvim_linter ?
   use_lint             = {},
+  -- TODO: add full setup abilitiy
+  -- TODO: RENAME use_nvim_treesitter ?
   use_treesitter       = {},
+
+  plugins_configs_default_templates = nil,
 
   start_dashboard      = {},
   keymaps              = {},
@@ -38,14 +46,20 @@ TinyNvimLoader = {
   end,
 
   load_vim_plug_bundle = function(self)
-    vim.o.rtp = vim.o.rtp .. self.vim_plug_bundle_path ..  '/Vundle.vim'
+    local bundle_path = self.vim_plug_bundle_path
+    local bundle      = self.vim_plug_bundle
+
+    if type(bundle_path) ~= "string" then return end
+    if type(bundle)      ~= "table"  then return end
+
+    vim.o.rtp = vim.o.rtp .. bundle_path ..  '/Vundle.vim'
 
     local vim  = vim
     local Plug = vim.fn['plug#']
 
-    vim.call('plug#begin', self.vim_plug_bundle_path)
+    vim.call('plug#begin', bundle_path)
 
-    for _, group_bundle in ipairs(self.vim_plug_bundle or {}) do
+    for _, group_bundle in ipairs(bundle) do
       local plugins = group_bundle.plugins or {}
 
       for _, plugin_data in ipairs(plugins) do
@@ -60,8 +74,11 @@ TinyNvimLoader = {
     vim.call('plug#end')
   end,
 
-  load_settings_files = function(self)
-    for _, setting_file in ipairs(self.main_settings_files) do
+  load_additional_config_files = function(self)
+    local opt = self.additional_vim_config_files
+    if type(opt) ~= "table" then return end
+
+    for _, setting_file in ipairs(opt) do
       vim.fn.execute('source ' .. self.vim_files_path() .. setting_file)
     end
   end,
@@ -72,27 +89,21 @@ TinyNvimLoader = {
       vim.opt.bg = self.bg
       vim.cmd.colorscheme(self.colorscheme)
     end
-
-    -- do not hl cursor in term
-    vim.cmd [[
-      hi CursorLine cterm=NONE
-    ]]
   end,
 
   log_reloading = function(self)
     if self.profile_loads > 1 then
-      print("TinyNvimLoader: reloading profile #" .. tostring(self.profile_loads))
+      print("NvimConfigLoader: reloading profile #" .. tostring(self.profile_loads))
     end
 
     self.profile_loads = self.profile_loads + 1
   end,
 
   load_vim_options = function(self)
-    -- vim.cmd [[
-    --  filetype off
-    -- ]]
+    local opt = self.vim_options
+    if type(opt) ~= "table" then return end
 
-    for option, v in pairs(self.vim_options) do
+    for option, v in pairs(opt) do
       local value
 
       if type(v) == 'function' then
@@ -106,7 +117,10 @@ TinyNvimLoader = {
   end,
 
   load_vim_globals = function(self)
-    for global, v in pairs(self.vim_globals) do
+    local opt = self.vim_globals
+    if type(opt) ~= "table" then return end
+
+    for global, v in pairs(opt) do
       local value
 
       if type(v) == 'function' then
@@ -120,7 +134,10 @@ TinyNvimLoader = {
   end,
 
   load_autocommands = function(self)
-    for group, commands in pairs(self.autocommands) do
+    local opt = self.autocommands
+    if type(opt) ~= "table" then return end
+
+    for group, commands in pairs(opt) do
       local augroup = vim.api.nvim_create_augroup(group, { clear = true })
 
       for _, cmd in ipairs(commands) do
@@ -135,30 +152,11 @@ TinyNvimLoader = {
   end,
 
   load_keymaps = function(self)
-    for _, group in pairs(self.keymaps) do
-      for _, mapping in ipairs(group) do
-        vim.keymap.set(unpack(mapping))
-      end
-    end
-  end,
+    local opt = self.keymaps
+    if type(opt) ~= "table" then return end
 
-  setup_rg = function(self)
-    if (self.use_rg) then
-      vim.opt.grepprg = 'rg --color=never'
-    end
-  end,
-
-  setup_zsh = function(self)
-    if (self.use_zsh and vim.fn.executable('/bin/zsh') == 1) then
-      vim.opt.shell = '/bin/zsh -l'
-    end
-  end,
-
-  setup_fzf = function(self)
-    if self.use_fzf then
-      vim.cmd [[
-        set rtp+=/usr/local/opt/fzf
-      ]]
+    for _, group in pairs(opt) do
+      for _, mapping in ipairs(group) do vim.keymap.set(unpack(mapping)) end
     end
   end,
 
@@ -227,6 +225,10 @@ TinyNvimLoader = {
     vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
   end,
 
+  setup_plugins_from_templates = function()
+    -- TODO:
+  end,
+
   setup = function(self, config)
     for k, v in pairs(config) do
       if k ~= 'setup' then self[k] = v end
@@ -235,15 +237,14 @@ TinyNvimLoader = {
     self:load_vim_plug_bundle()
     self:load_vim_options()
     self:load_vim_globals()
-    self:load_settings_files()
+
+    self:load_additional_config_files()
 
     if type(config.setup) == 'function' then config.setup(self) end
 
     self:load_keymaps()
     self:load_autocommands()
-    self:setup_rg()
-    self:setup_zsh()
-    self:setup_fzf()
+    -- TODO: IDEA: move setup to loadable loaders
     self:setup_linter()
     self:setup_treesitter()
     self:setup_start_dashboard()
@@ -252,4 +253,79 @@ TinyNvimLoader = {
   end
 }
 
-return TinyNvimLoader
+local plugins_configs_default_templates = {
+  use_nvim_lint = {
+    setup = function(self)
+      if type(self.use_lint) == 'table' then
+        if type(self.use_lint.linters_by_ft) == 'table' then
+          require('lint').linters_by_ft = self.use_lint.linters_by_ft
+
+          local pattern = self.use_lint.file_pattern or { "*.rb", "*.erb", "*.haml", "*.slim" }
+
+          vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+            pattern = pattern,
+            callback = function()
+              require("lint").try_lint()
+            end,
+          })
+
+          vim.api.nvim_create_autocmd({ "BufRead" }, {
+            pattern = pattern,
+            callback = function()
+              require("lint").try_lint()
+            end,
+          })
+        end
+      end
+    end,
+
+    use_nvim_treesitter = {
+      setup = function(self)
+        if type(self.use_treesitter) ~= 'table' or type(self.use_treesitter.languages) ~= 'table' then return end
+
+        require('nvim-treesitter.configs').setup {
+          ensure_installed = self.use_treesitter.languages,
+          sync_install     = true,
+          auto_install     = true,
+          highlight        = { enable = true }
+        }
+      end
+    },
+
+    use_alpha_dashboard = {
+      setup = function(self)
+        local alpha = require("alpha")
+        local startify = require("alpha.themes.startify")
+
+        local title = self.start_dashboard.title or 'Hello world'
+
+        startify.section.header.val = { '[[> ' .. title  .. ' ]]' }
+
+        startify.opts.layout[1].val = 2
+        startify.opts.opts.margin   = 3
+
+        -- disable MRU
+        startify.section.mru.val = { { type = "padding", val = 0 } }
+
+        local buttons = {}
+
+        for _, data in pairs(self.start_dashboard.buttons or {}) do
+          local btn = startify.button(unpack(data))
+          table.insert(buttons, btn)
+        end
+
+        startify.section.top_buttons.val = buttons
+
+        -- Send config to alpha
+        alpha.setup(startify.config)
+
+        -- Disable folding on alpha buffer
+        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+      end
+    }
+  }
+}
+
+NvimConfigLoader.plugins_configs_default_templates = plugins_configs_default_templates
+
+return NvimConfigLoader
